8d0cf3cae101db700832bedbac1b643fbcdb4eb2
gowtham123-pro <gowthammdb7@gmail.com>
2021-05-15 13:52:01 +0530

    updated ui with location sharing feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,51 @@
 const socket=io()
+//elements
 
+const $messegeform=document.querySelector('#form')
+const $messegefrominput=$messegeform.querySelector('#msg')
+const $messegeformbutton=$messegeform.querySelector('#but')
+const $locationbut=document.querySelector("#loc")
+const $messeags=document.querySelector('#mg-container')
+//template variables
+
+const msgs=document.querySelector("#message-template").innerHTML
+const loci=document.querySelector("#location-template").innerHTML
 socket.on('welcome',(msg)=>{
-    console.log(msg)
+      console.log(msg)
+      const html=Mustache.render(msgs,{message:msg})
+      $messeags.insertAdjacentHTML('beforeend',html)
+})
+socket.on('Locator',(loc)=>{
+    console.log("My location ",loc)
+    const html=Mustache.render(loci,{loc})
+    $messeags.insertAdjacentHTML('beforeend',html)
+})
+$messegeform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    $messegeformbutton.setAttribute('disabled','disabled')
+    socket.emit('texted',e.target.elements.msg.value,(error)=>{
+        $messegeformbutton.removeAttribute('disabled')
+        $messegefrominput.value=''
+        $messegefrominput.focus()
+        if(error){
+           return  console.log(error)
+        }
+        console.log('msg has been deliverd ')
+    })
+
 })
 
-document.querySelector('#form').addEventListener('submit',(e)=>{
-    e.preventDefault()
-    socket.emit('texted',e.target.elements.msg.value)
 
+
+document.querySelector("#loc").addEventListener('click',()=>{
+    $locationbut.setAttribute('disabled','disabled')
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by ur browser')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('location',[position.coords.latitude,position.coords.longitude],()=>{
+            console.log('Location shared') 
+        })
+    })
+    $locationbut.removeAttribute('disabled')
 })
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const socket=require('socket.io')
 const http=require('http')
 const app=exp()
 const server=http.createServer(app)
+const Filter = require('bad-words')
 
 const io=socket(server)
 
@@ -16,11 +17,25 @@ app.use(exp.static(public))
 
 io.on('connection',(socket)=>{
     console.log("New web socket connection")
-    socket.emit('welcome',"welcome user")
-    socket.on('texted',(msg)=>{
+    socket.emit('welcome',"welcome User")
+    socket.broadcast.emit('welcome','a new user has joined')
+    socket.on('texted',(msg,callback)=>{
+        const filter=new Filter()
+        if(filter.isProfane(msg)){
+            return callback('profanilty is not allowed')
+        }
         io.emit('welcome',msg)
+        callback()
+    })
+    socket.on('disconnect',()=>{
+        io.emit('welcome','A user has left')
+    })
+    socket.on('location',(loc,callback)=>{
+        io.emit('Locator','https://www.google.com/maps/?q='+loc[0]+','+loc[1]+','+'21z')
+        callback()
     })
 })
+
 
 
 
